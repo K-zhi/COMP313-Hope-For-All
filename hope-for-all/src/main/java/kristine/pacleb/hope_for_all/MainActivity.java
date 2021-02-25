8ff7e069a5f60a7b346f69bac581e5b5f7d96d70
kristine <codewithtine@gmail.com>
2021-02-25 17:11:35 -0500

    First commit - Created UI for Registration.

---
+++ hope-for-all/src/main/java/kristine/pacleb/hope_for_all/MainActivity.java
@@ -0,0 +1,14 @@
+package kristine.pacleb.hope_for_all;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
