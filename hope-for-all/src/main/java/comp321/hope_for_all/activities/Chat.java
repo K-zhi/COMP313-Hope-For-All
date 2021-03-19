7a20d0d0017cc636fe49e3d641290d097ec1bbcc
haile <hailey0882@gmail.com>
2021-03-19 08:32:32 -0400

    Created Data Transfer Object

--- hope-for-all/src/main/java/comp321/hope_for_all/activities/Chat.java
+++ hope-for-all/src/main/java/comp321/hope_for_all/activities/Chat.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import comp321.hope_for_all.R;
 
 public class Chat extends AppCompatActivity {
+    FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = mDatabase.getReference("Message");
+
+        myRef.setValue("Hello, World");
     }
 }
