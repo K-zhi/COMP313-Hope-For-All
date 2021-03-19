1f27d89a2ec5b2143bd1c7e040584c78c1dcabe0
haile <hailey0882@gmail.com>
2021-03-19 19:13:03 -0400

    Testing to connect DB and stored the chat data

--- hope-for-all/src/main/java/comp321/hope_for_all/activities/Chat.java
+++ hope-for-all/src/main/java/comp321/hope_for_all/activities/Chat.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -44,6 +45,10 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        Intent intent = getIntent();
+        if(intent.getExtras().getString("UserName") != null)
+            nick = intent.getExtras().getString("UserName");
 
         btnSendChat = findViewById(R.id.btnSendChat);
         editTxtChat = findViewById(R.id.editTxtChat);
--- hope-for-all/src/main/java/comp321/hope_for_all/activities/Message.java
+++ hope-for-all/src/main/java/comp321/hope_for_all/activities/Message.java
@@ -6,17 +6,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import comp321.hope_for_all.R;
 
-public class Message extends AppCompatActivity {
+public class Message extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseUser user;
+    private DatabaseReference databaseReference;
+    private String nickName;
+
+    private FloatingActionButton fabMain, fabSub1, fabSub2;
+    private Animation fab_open, fab_close;
+    private boolean isFabOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        Intent intent = getIntent();
+        if(intent.getExtras().getString("UserName") != null)
+            nickName = intent.getExtras().getString("UserName");
+
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+
+        fabMain = (FloatingActionButton) findViewById(R.id.FloatingBtnMain);
+        fabMain.setOnClickListener(this);
 
         bottomNavigation();
     }
@@ -46,5 +71,35 @@ public class Message extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.FloatingBtnMain :
+                Intent intent = new Intent(getApplicationContext(), Chat.class);
+                intent.putExtra("UserName", nickName);
+                startActivity(intent);
+
+                break;
+        }
+    }
+
+    private void toggleFab() {
+        if(isFabOpen) {
+            fabMain.setImageResource(R.drawable.ic_chat);
+            fabSub1.startAnimation(fab_close);
+            fabSub2.startAnimation(fab_close);
+            fabSub1.setClickable(false);
+            fabSub2.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fabMain.setImageResource(R.drawable.ic_close);
+            fabSub1.startAnimation(fab_open);
+            fabSub2.startAnimation(fab_open);
+            fabSub1.setClickable(true);
+            fabSub2.setClickable(true);
+            isFabOpen = true;
+        }
     }
 }
