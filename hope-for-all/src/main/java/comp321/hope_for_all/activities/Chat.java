62a1897f0470c76bc0c248c4c1a09adb50f667f8
haile <hailey0882@gmail.com>
2021-03-19 08:24:20 -0400

    Created chat activity and layout

---
+++ hope-for-all/src/main/java/comp321/hope_for_all/activities/Chat.java
@@ -0,0 +1,16 @@
+package comp321.hope_for_all.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp321.hope_for_all.R;
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
