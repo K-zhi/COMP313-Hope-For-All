8d3bef2d6f8f9971b0686dea326f3ed04c6e2bf7
kristine <codewithtine@gmail.com>
2021-03-30 14:07:24 -0400

    CounselorMessage

--- hope-for-all/src/main/java/comp321/hope_for_all/activities/CounselorMessage.java
+++ hope-for-all/src/main/java/comp321/hope_for_all/activities/CounselorMessage.java
@@ -41,6 +41,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
 import com.google.protobuf.StringValue;
 
 import java.util.ArrayList;
