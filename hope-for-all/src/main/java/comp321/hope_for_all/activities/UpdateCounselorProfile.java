1d31b14fb59e08d01722adc1d17e9756f3a603d4
kristine <codewithtine@gmail.com>
2021-03-30 16:03:36 -0400

    Redesigning User and Counselor Profile activities.

--- hope-for-all/src/main/java/comp321/hope_for_all/activities/UpdateCounselorProfile.java
+++ hope-for-all/src/main/java/comp321/hope_for_all/activities/UpdateCounselorProfile.java
@@ -1,8 +1,5 @@
 package comp321.hope_for_all.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
@@ -14,6 +11,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
@@ -31,7 +31,7 @@ import comp321.hope_for_all.adapter.PlaceAutoSuggestAdapter;
 
 public class UpdateCounselorProfile extends AppCompatActivity {
 
-    private EditText name, bio, email, website; //location;
+    private EditText name, bio, email, website;
     private AutoCompleteTextView autoCompleteTextView;
     private Button save, cancel;
 
