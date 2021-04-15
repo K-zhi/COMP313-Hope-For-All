8b888ae5d8399e0f2c1400e2a570edcdbcf15fd5
navjotsengh <72287772+navjotsengh@users.noreply.github.com>
2021-04-15 10:00:04 -0400

    TC 12

--- hope-for-all/src/androidTest/java/comp321/hope_for_all/CounselorDeletePostTest.java
+++ hope-for-all/src/androidTest/java/comp321/hope_for_all/CounselorDeletePostTest.java
@@ -3,7 +3,11 @@ package comp321.hope_for_all;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -12,11 +16,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import comp321.hope_for_all.activities.MainActivity;
 import comp321.hope_for_all.activities.MainCounselor;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -25,8 +31,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class CounselorDeletePostTest {
     @Rule
-    public ActivityTestRule<MainCounselor> mActivityTestRule =
-            new ActivityTestRule<>(MainCounselor.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
 
 
     @Before
@@ -44,11 +50,27 @@ public class CounselorDeletePostTest {
                 });
     }
     @Test
-    public void DeleteCounselerPost(UiController uiController, View view)
-    {
+    public void DeletePost() {
 
-        View del = view.findViewById(R.id.delete_post);
-        del.performClick();
+        ViewAction del = new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View del = view.findViewById(R.id.delete_post);
+                del.performClick();
+            }
+        };
+
+        ViewInteraction recyclerViewInteraction = Espresso.onView(ViewMatchers.withId(R.id.recycler_view));
     }
 
 
